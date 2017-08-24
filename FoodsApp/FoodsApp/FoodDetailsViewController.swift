d27973ab31b9709d1bbe89835f0b0123d54d2aff
BalioFVFX <vfxbaliof@gmail.com>
2017-08-24 13:14:55 +0300

    FoodDetailsViewController
    Created new view controller with elements and basic constraints

---
+++ FoodsApp/FoodsApp/FoodDetailsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FoodDetailsViewController.swift
+//  FoodsApp
+//
+//  Created by Erik Baliov on 24.08.17.
+//  Copyright © 2017 None. All rights reserved.
+//
+
+import UIKit
+
+class FoodDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
