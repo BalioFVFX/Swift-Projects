0408cfcbda55adc18ac698fe565fa321fea46b8d
BalioFVFX <vfxbaliof@gmail.com>
2017-08-24 16:00:52 +0300

    New section
    Added section "My recipes"

--- FoodsApp/FoodsApp/FoodsTableViewController.swift
+++ FoodsApp/FoodsApp/FoodsTableViewController.swift
@@ -42,21 +42,42 @@ class FoodsTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return LocalDataManager.myFoods.count
+        switch section {
+        case 0:
+            return 2
+        default:
+            return LocalDataManager.myFoods.count
+        }
+        
     }
 
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+
+        
+        switch section {
+        case 0:
+            return "My recipes"
+        default:
+            return "All recipes"
+        }
+    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "FoodsTableViewCell", for: indexPath) as! FoodsTableViewCell
-    
+        
+        if(indexPath.section == 0){
+            cell.foodItemRecipeNameLabel.text = "First Section"
+        }
+        
+        if(indexPath.section == 1){
         cell.foodItemRecipeNameLabel.text = LocalDataManager.myFoods[indexPath.row].recipeName
         cell.foodItemRecipeDurationLabel.text = LocalDataManager.myFoods[indexPath.row].recipeTimeToCook
         cell.foodItemImageView.image = UIImage(named: LocalDataManager.myFoods[indexPath.row].recipeImageName)
-        
+        }
         return cell
         
     }
