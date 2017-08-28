04e3c3029d0070bdc6afd359c428e5638846438d
BalioFVFX <vfxbaliof@gmail.com>
2017-08-28 17:03:20 +0300

    Comments update

---
+++ FoodsApp/FoodsApp/CommentDetailsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  CommentDetailsViewController.swift
+//  FoodsApp
+//
+//  Created by Erik Baliov on 28.08.17.
+//  Copyright © 2017 None. All rights reserved.
+//
+
+import UIKit
+
+class CommentDetailsViewController: UIViewController {
+
+    var currentCell:Int = 0
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var commentTextView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.nameLabel.text = LocalDataManager.currentFood.recipeUser
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- FoodsApp/FoodsApp/CommentsTableViewController.swift
+++ FoodsApp/FoodsApp/CommentsTableViewController.swift
@@ -42,6 +42,7 @@ class CommentsTableViewController: UITableViewController {
                 SVProgressHUD.dismiss(withDelay:0.7)
             }
         }
+        print(LocalDataManager.currentFood.recipeUser)
     }
     
     override func didReceiveMemoryWarning() {
@@ -65,7 +66,7 @@ class CommentsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CommentsTableViewCell", for: indexPath) as! CommentsTableViewCell
 
-        cell.commentLabel.text = "Text"
+        cell.commentLabel.text = LocalDataManager.currentFood.recipeUser
         cell.commentNameLabel.text = LocalDataManager.currentFood.comments[indexPath.row]
         cell.commentImageView.image = UIImage(named: "food.png")
         cell.commentDateLabel.text = "Date"
@@ -73,55 +74,51 @@ class CommentsTableViewController: UITableViewController {
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        self.tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
     
     @IBAction func addCommentBarButtonTapped(_ sender: UIBarButtonItem) {
         self.performSegue(withIdentifier: "addCommentSegue", sender: nil)
     }
     
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    // MARK: - SEGUE
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        guard let segueIdentifier = segue.identifier else{
+            return
+        }
+        
+        switch segueIdentifier{
+        case "commentDetailsSegue":
+            
+            guard let cell = sender as? UITableViewCell else{
+                assert(false)
+                return
+            }
+            
+            guard let indexPath = self.tableView.indexPath(for: cell) else{
+                return
+            }
+            
+//            if(indexPath.section == 0){
+//                (segue.destination as! FoodDetailsViewController).currentCellItem = LocalDataManager.myFoods[indexPath.row]
+//            }
+//            if(indexPath.section == 1){
+//                (segue.destination as! FoodDetailsViewController).currentCellItem = LocalDataManager.allFoods[indexPath.row]
+//            }
+            
+            (segue.destination as! CommentDetailsViewController)
+            
+            print(indexPath.row)
+            
+        default:
+            break
+            
+        }
+        
     }
-    */
-
 }
